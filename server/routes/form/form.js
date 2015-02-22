1818005f34f0f5f807d9ded9427a04952dfc541d
Kristiyan Dzhamalov <kdzhamal@ucsd.edu>
2015-02-22 01:42:07 -0800

    Added delete

--- server/routes/form/form.js
+++ server/routes/form/form.js
@@ -1,50 +1,46 @@
 'use strict';
 
-/* This module is meant to house all of the API 
+/* This module is meant to house all of the API
  * routes that pertain to users
  */
 var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
 var urlparser = bodyparser.urlencoded({extended: false});
-var mongoose = require('mongoose');
-var templateForm = require('../../models/form/FormTemplate');
+
 
 
 /********** FORM TEMPLATE ROUTES **********/
+
 router.get('/form/template/:id', function(req, res) {
-  templateForm.findOne({'_id' : req.params.id}, function(err, template) {
-    if(err)
-      res.json({error: "There was an error finding the template form."});
-    else
-      res.send(template);
-  });
+
 });
 
+/* accept DELETE request at /form/template/:template_id
+ * {
+ * error: [string],
+ * template_id : [integer],
+ * company_id : [integer],
+ * template : [object]
+ * }
+ */
+router.delete('/form/template/:template_id', urlparser, function (req, res) {
+    /* Get id param from request */
+    var templateID = req.params.id;
 
-router.get('/form/template/company/:id', function(req, res) {
-  templateForm.findOne({'_admin_id' : req.params.id}, function(err, template) {
-    if(err)
-      res.json({error: "There was an error finding the template form."});
-    else
-      res.send(template);
-  });
-});
+    if(!templateID) {
+      res.status(400).send('need a template id');
+      return;
+    }
 
-
-router.post('/form/template', function(req, res) {
-  var newTemplate = new form();
-  newTemplate._admin_id = req.body._admin_id;
-  newTemplate.template = req.body.template;
-
-  newTemplate.save(function(err, template) {
-    if(err)
-      res.json({error: "There was an error inserting a new template."});
-    else
-      res.json(template);
-  });
-});
-
+    FormTemplate.findOneAndRemove({_id: templateID}, function(err, result) {
+      if(err) {
+        res.status(500).send('There was problem removing the form template');
+        return;
+      }
+      res.send('removed form template: ' + JSON.stringify(result));
+    });
+})
 
 
 
