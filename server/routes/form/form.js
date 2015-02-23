8e484d3ed295be4d9fd3b9d68f61314a99462597
Michael Cieplak <michael.cieplak@gmail.com>
2015-02-22 23:37:54 -0800

    Initial version of retrieve functions

--- server/routes/form/form.js
+++ server/routes/form/form.js
@@ -7,13 +7,27 @@ var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
 var urlparser = bodyparser.urlencoded({extended: false});
+var mongoose = require('mongoose');
 
 
 
 /********** FORM TEMPLATE ROUTES **********/
-
 router.get('/form/template/:id', function(req, res) {
+  mongoose.model('FormTemplate').findById(req.params.id, function(err, template) {
+    if(err)
+      res.json(err);
 
+    res.send(template);
+  });
+});
+
+router.get('/form/template/company/:id', function(req, res) {
+  mongoose.model('FormTemplate').findById(req.params._admin_id, function(err, template) {
+    if(err)
+      res.json(err);
+
+    res.send(template);
+  });
 });
 
 
