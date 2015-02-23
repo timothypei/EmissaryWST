d093194fe8fd840e73c266616588d9c3076ae600
Luoxiao <lul017@Ucsd.edu>
2015-02-22 18:53:25 -0800

    Fixed error message

--- client/app/components/receptionistPortal/signin/controllers/signin-controller.js
+++ client/app/components/receptionistPortal/signin/controllers/signin-controller.js
@@ -3,11 +3,8 @@
 angular.module('signin')
   .controller('SigninController', ['$scope', 'SigninService', function($scope, signinService){
   	$scope.user = {email: '', password: ''};
-
-    $scope.title = 'Top Sellers in Books'; 
+    $scope.err = false; 
 
   	$scope.login = function(){
-  		$scope.err = true;
-  		console.log("fjdklsa");
   	};
   }]);
