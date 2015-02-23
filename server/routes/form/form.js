e9442089d971281f1e813f58d58a6e9d009d8606
Allen Shih <Allen@Allens-MacBook-Pro-2.local>
2015-02-23 00:25:00 -0800

    GET/POST calls for Patient Forms, added testing

--- server/routes/form/form.js
+++ server/routes/form/form.js
@@ -1,13 +1,13 @@
 'use strict';
 
-/* This module is meant to house all of the API 
+/* This module is meant to house all of the API
  * routes that pertain to users
  */
 var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
 var urlparser = bodyparser.urlencoded({extended: false});
-
+var SubmittedForm = require('../../models/form/SubmittedForm');
 
 
 /********** FORM TEMPLATE ROUTES **********/
@@ -22,10 +22,74 @@ router.get('/form/template/:id', function(req, res) {
 
 /********** PATIENT FORM ROUTES **********/
 
-router.get('/form/patient/:form_id', function(req, res) {
 
-});
+var getSubmittedFormById = function(req, res) {
+  SubmittedForm.findOne({ '_id': req.params.form_id }, function (err, submittedForm) {
+    if (err) {
+      res.json({error: "An error occured while finding patient form"});
+      return;
+    }
+    res.json(submittedForm)
+  });
+};
 
+var postSubmittedForm = function(req, res) {
+  var form = new SubmittedForm();
+  form.form = req.body.form;
+  form._admin_id = req.body._admin_id;
+  form.firstName = req.body.firstName;
+  form.lastName = req.body.lastName;
+  form.email = req.body.email;
+  form.date = new Date();
+  form.save(function(err, savedForm){
+    if (err){
+      res.json({error: "An error occured while saving the submitted form"})
+      return;
+    }
+    res.json(savedForm);
+  });
+}
+
+var getSubmittedFormByPatientInfo = function(req, res) {
+  var query = {},
+    firstName = req.query.firstName,
+    lastName = req.query.lastName,
+    email = req.query.email;
+
+  if(!((firstName && lastName) || email)) {
+    res.json({error: "You must specify either both first and last name or email"});
+    return;
+  }
+  firstName ? query.firstName = firstName : null;
+  lastName ? query.lastName = lastName : null;
+  email ? query.email = email : null;
+
+  if(req.query.mostRecent == "true") {
+    SubmittedForm.findOne(query).sort('-date').exec(function (err, submittedForm) {
+      if (err) {
+        res.json({error: "An error occured while finding patient form"});
+        return;
+      }
+      res.json(submittedForm);
+    });
+  }
+  else {
+    SubmittedForm.find(query, function(err, submittedForms) {
+      if (err) {
+        res.json({error: "An error occured while finding patient forms"});
+        return;
+      }
+      res.json(submittedForms);
+    });
+  }
+
+
+
+};
+
+router.get('/form/patient/:form_id', getSubmittedFormById);
+router.get('/form/patient', getSubmittedFormByPatientInfo)
+router.post('/form/patient', postSubmittedForm);
 
 
 
--- server/test/form.js
+++ server/test/form.js
@@ -9,3 +9,199 @@
 
 
 /********** PATIENT FORM TESTING **********/
+var submittedForm = {
+  "form_id": "1",
+  "form_name": "My Test Form",
+  "form_fields": {
+    "1": {
+      "field_id": 1,
+      "field_title": "First Name",
+      "field_type": "textfield",
+      "field_value": "John",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "2": {
+      "field_id": 2,
+      "field_title": "Last Name",
+      "field_type": "textfield",
+      "field_value": "Doe",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "3": {
+      "field_id": 3,
+      "field_title": "Gender",
+      "field_type": "radio",
+      "field_value": "1",
+      "field_required": true,
+      "field_disabled": false,
+      "field_options": [
+        {
+          "option_id": 1,
+          "option_title": "Male",
+          "option_value": 1
+        },
+        {
+          "option_id": 2,
+          "option_title": "Female",
+          "option_value": 2
+        }
+      ]
+    },
+    "4": {
+      "field_id": 4,
+      "field_title": "Email Address",
+      "field_type": "email",
+      "field_value": "test@example.com",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "5": {
+      "field_id": 5,
+      "field_title": "Password",
+      "field_type": "password",
+      "field_value": "hello",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "6": {
+      "field_id": 6,
+      "field_title": "Birth Date",
+      "field_type": "date",
+      "field_value": "01.21.1980",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "7": {
+      "field_id": 7,
+      "field_title": "Your browser",
+      "field_type": "dropdown",
+      "field_value": "3",
+      "field_required": false,
+      "field_disabled": false,
+      "field_options": [
+        {
+          "option_id": 1,
+          "option_title": "-- Please Select --",
+          "option_value": 1
+        },
+        {
+          "option_id": 2,
+          "option_title": "Internet Explorer",
+          "option_value": 2
+        },
+        {
+          "option_id": 3,
+          "option_title": "Google Chrome",
+          "option_value": 3
+        },
+        {
+          "option_id": 4,
+          "option_title": "Mozilla Firefox",
+          "option_value": 4
+        }
+      ]
+    },
+    "8": {
+      "field_id": 8,
+      "field_title": "Additional Comments",
+      "field_type": "textarea",
+      "field_value": "test",
+      "field_required": false,
+      "field_disabled": false
+    },
+    "9": {
+      "field_id": 9,
+      "field_title": "I accept the terms and conditions.",
+      "field_type": "checkbox",
+      "field_value": "1",
+      "field_required": true,
+      "field_disabled": false
+    },
+    "10": {
+      "field_id": 10,
+      "field_title": "I have a secret.",
+      "field_type": "hidden",
+      "field_value": "X",
+      "field_required": false,
+      "field_disabled": false
+    }
+  },
+  "submitted": true
+}
+
+var submittedFormId = null;
+var should = require('chai').should();
+var asser = require('chai').assert;
+var request = require('supertest');
+var mongoose = require('mongoose');
+
+var config = require('../config/config');
+
+var AdminUser = require('../models/Authmodel')
+
+describe('Submitted Patient Form', function(){
+  var url = "localhost:" + config.port;
+
+  var admin = null;
+  before(function(done){
+    mongoose.connect(config.mongoDBUrl);
+    var newAdmin = new AdminUser();
+    newAdmin.local.email = "test@test.com";
+    newAdmin.local.password = "password_secret";
+    newAdmin.save(function(err, savedAdmin){
+      if (err){
+        throw(err);
+        return;
+      }
+      admin = savedAdmin;
+      done();
+    });
+  });
+
+  describe('POST /api/form/patient', function(){
+    it('should save submitted form', function(done){
+      request(url)
+        .post('/api/form/patient')
+        .send({
+          _admin_id: admin._id,
+          form: submittedForm,
+          firstName: "Jimbo",
+          lastName: "Cruise",
+          email: "jcruise@tomcruise.com"
+        })
+        .end(function(err, res){
+          //console.log(err);
+          //console.log(res);
+          res.body.should.have.property('form').and.be.instanceof(Object);
+          res.body.should.have.property('_admin_id').and.be.equal(''+admin._id);
+          submittedFormId = res.body._id;
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/form/:form_id', function(){
+    it('should respond with submitted form data', function(done){
+      request(url)
+        .get('/api/form/patient/' + submittedFormId)
+        .end(function(err, res){
+          res.body.should.have.property('_id');
+          res.body.should.have.property('firstName');
+          res.body.should.have.property('lastName');
+          res.body.should.have.property('email');
+          res.body.should.have.property('_admin_id');
+          res.body.should.have.property('date');
+          res.body.should.have.property('form').and.be.instanceof(Object);
+
+          res.body.form.should.deep.equal(submittedForm);
+          res.body._id.should.equal(submittedFormId);
+          done();
+        });
+    });
+  });
+
+
+
+});
