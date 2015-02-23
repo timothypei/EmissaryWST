2bf800ee7179b26965362a036233f65678e1ad70
Luoxiao <lul017@Ucsd.edu>
2015-02-23 10:50:27 -0800

    intial setup of services

--- client/app/components/receptionistPortal/signin/controllers/signin-controller.js
+++ client/app/components/receptionistPortal/signin/controllers/signin-controller.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('signin')
-  .controller('SigninController', ['$scope', 'SigninService', function($scope, signinService){
+  .controller('SigninController', ['$scope', 'SigninService', function($scope, SigninService){
   	$scope.user = {email: '', password: ''};
     $scope.err = false; 
-
   	$scope.login = function(){
+  		var account = this;
+  		account.dat = {};
+  		SigninService.login($scope.user)
+      	.success(function(data){
+      		console.log("fjklsdaf");
+        	account.dat = data;
+        	return data;
+      	})
+      	.error(function(err){
+       		console.log("There was an error: " + err);
+       		return err;
+     	});
   	};
+  
   }]);
--- client/app/components/receptionistPortal/signin/services/signin-services.js
+++ client/app/components/receptionistPortal/signin/services/signin-services.js
@@ -2,5 +2,8 @@
 
 angular.module('signin')
   .service('SigninService', ['$http', function($http) {
-      
+      this.login = function(user) {
+        console.log($http.POST('https://blue-jay.herokuapp.com/api/login', user));
+        return $http.POST('https://blue-jay.herokuapp.com/api/login', user);
+      };
   }]);
