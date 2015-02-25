ad7e69627f023c2d7de9161a7af2373d65cddf4d
xxvii27 <llesmana@ucsd.edu>
2015-02-25 00:00:22 -0800

    pass test 3

--- server/routes/theme.js
+++ server/routes/theme.js
@@ -17,91 +17,91 @@ var Theme = require('../models/Theme');
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-    // do logging
-    console.log('Intializing.....');
-    next(); 
+  // do logging
+  console.log('Intializing.....');
+  next();
 });
 
 
 //For client access
 router.route('/:user_id/theme')
 
-  	    //post with default values
-	     .post(function(req, res) {
-	        
-		       var theme = new Theme();    
-		       theme.user_id = req.params.user_id;//company or user id
-		       theme.form_color = "default"
-		       theme.background_img =  "default";
-		       theme.displayPhone = false;
-		       theme.displayClock = false;
-		       theme.displaySignature = false;
-		       theme.additionalComments = false;
+  //post with default values
+  .post(function(req, res) {
 
-		        theme.save(function(err) {
-		            if (err)
-		                res.send(err);
+    var theme = new Theme();
+    theme.user_id = req.params.user_id;//company or user id
+    theme.form_color = "default";
+    theme.background_img = "default";
+    theme.displayPhone = false;
+    theme.displayClock = false;
+    theme.displaySignature = false;
+    theme.additionalComments = false;
 
-		            res.json(theme);
-		        });
-	        
-	     })
+    theme.save(function(err) {
+      if(err)
+        res.send(err);
 
-	    //get the theme correspond to the user
-	     .get(function(req, res) {
-		        Theme.findOne({user_id: req.params.user_id}, function(err, theme) {
-		            if (err)
-		                res.send(err);
+      res.json(theme);
+    });
 
-		            res.json(theme);
-		        });
-	      })
+  })
 
-	      //Edit, when the user save new settings
-                 .put(function(req, res) {
+  //get the theme correspond to the user
+  .get(function(req, res) {
+    Theme.findOne({user_id: req.params.user_id}, function(err, theme) {
+      if(err)
+        res.send(err);
 
-		      Theme.findOne({user_id: req.params.user_id}, function(err, theme) {
+      res.json(theme);
+    });
+  })
 
-                                     if (err)
-		                res.send(err);
+  //Edit, when the user save new settings
+  .put(function(req, res) {
 
-		              theme.user_id = req.params.user_id;//company or user id
-                                      if(req.body.form_color )
-                                            theme.form_color = req.body.form_color;
-                                      if(req.body.background_img )
-                                            theme.background_img =  req.body.background_img;
-                                      if(req.body.displayPhone )
-                                            theme.displayPhone = req.body.displayPhone;
-                                      if(req.body.displayClock )
-                                            theme.displayClock = req.body.displayClock;
-                                      if(req.body.displaySignature )
-                                            theme.displaySignature = req.body.displaySignature;
-                                      if(req.body.additionalComments )
-                                            theme.additionalComments = req.body.additionalComments;
-		              
-                                      theme.save(function(err) {
-		                if (err)
-		                    res.send(err);
+    Theme.findOne({user_id: req.params.user_id}, function(err, theme) {
 
-		                  res.json(theme);
-		              });
+      if(err)
+        res.send(err);
 
-		     });
-	      })
+      theme.user_id = req.params.user_id;//company or user id
+      if(req.body.form_color)
+        theme.form_color = req.body.form_color;
+      if(req.body.background_img)
+        theme.background_img = req.body.background_img;
+      if(req.body.displayPhone)
+        theme.displayPhone = req.body.displayPhone;
+      if(req.body.displayClock)
+        theme.displayClock = req.body.displayClock;
+      if(req.body.displaySignature)
+        theme.displaySignature = req.body.displaySignature;
+      if(req.body.additionalComments)
+        theme.additionalComments = req.body.additionalComments;
 
-                 //Delete, when a user unsuscribed from the service
-                 .delete(function(req, res){
+      theme.save(function(err) {
+        if(err)
+          res.send(err);
 
-                            Theme.remove({
-                                        user_id: req.params.user_id
-                                    }, function(err, theme) {
-                                        if (err)
-                                            res.send(0);
-                                        else
-                                            res.send(1);
-                            });
+        res.json(theme);
+      });
+
+    });
+  })
+
+  //Delete, when a user unsuscribed from the service
+  .delete(function(req, res) {
+
+    Theme.remove({
+      user_id: req.params.user_id
+    }, function(err, theme) {
+      if(err)
+        res.send(err);
+
+      res.json(theme);
+    });
 
 
-                 });
+  });
 
 module.exports = router;
