9437f6d638a02a4a382bf23f67336f18f89a623c
Luoxiao <lul017@Ucsd.edu>
2015-02-25 09:02:49 -0800

    Updated Api call

--- client/app/components/receptionistPortal/signin/controllers/signin-controller.js
+++ client/app/components/receptionistPortal/signin/controllers/signin-controller.js
@@ -3,22 +3,27 @@
 angular.module('signin')
   .controller('SigninController', ['$scope', '$location', 'SigninService', function($scope, $location, SigninService){
   	$scope.user = {email: '', password: ''};
-    $scope.err = false; 
+    $scope.errMessage ='';
     //this function is called when we press the login button
   	$scope.login = function(){
-  		var account = this;
-  		account.dat = {};
-      //calls the API to login
-  		SigninService.login($scope.user)
-      	.success(function(data){
-        	account.dat = data;
-          //redirects to the person's home page when a success
-        	$location.path(data.successRedirect);
-        	return data;
-      	})
-      	.error(function(err){
-       		$scope.err = true; 
-       		return err;
-     	});
+      if($scope.user.email.indexOf('@')==-1||$scope.user.email.indexOf('.')){
+        $scope.errMessage = 'Invaldi Email/Password'
+      }
+      else{
+  		  var account = this;
+  		  account.dat = {};
+        //calls the API to login
+  		  SigninService.login($scope.user)
+      	 .success(function(data){
+        	 account.dat = data;
+            //redirects to the person's home page when a success
+        	 $location.path(data.successRedirect);
+        	 return data;
+      	 })
+      	 .error(function(err){
+       	  	$scope.errMessage = 'Invalid Email/Password'; 
+       	  	return err;
+     	  });
+      }
   	};
   }]);
