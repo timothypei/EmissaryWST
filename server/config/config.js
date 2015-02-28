7ccb5196807c498e84ef6b3b72dc44ba764faf67
Kristiyan Dzhamalov <kdzhamal@ucsd.edu>
2015-02-27 18:54:26 -0800

    reverted the commit

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,6 @@
  * Config var for app
 **/
 module.exports = {
-  //mongoDBUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/test',
-  mongoDBUrl: process.env.MONGODB_URL || 'mongodb://bluejay:toomanysecrets@ds047581.mongolab.com:47581/robobetty-test',
+  mongoDBUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/test',
   port: process.env.PORT || 4941
 };
