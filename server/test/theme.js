b3db07fbd1914bda9327d8e5c6ebcd9bb5b89303
xxvii27 <llesmana@ucsd.edu>
2015-02-28 15:09:52 -0800

    apply auth with routing test

--- server/test/theme.js
+++ server/test/theme.js
@@ -2,6 +2,7 @@ var chai = require('chai');
 var should = chai.should();
 var Theme = require('../models/Theme');
 var config = require('../config/config');
+var ConfigureAuth = require('./server/test/ConfigureAuth.js');
 var request = require('supertest');
 
 //Schema Test
@@ -75,125 +76,152 @@ describe('Theme Settings Model', function() {
 
 //Route Tests need to be changed to work with auth
 
-/*
-describe('POST /api/:user_id/theme', function() {
-    it('should respond with theme info for respective user_id settings that were created for first time user', function(done) {
-        var url = "localhost:" + config.port;
-        var _user_id = '1';
-        var _form_color = 'default';
-        var _background_img = 'default';
-        var _displayPhone = false;
-        var _displayClock = false;
-        var _displaySignature = false;
-        var _additionalComments = false;
-        request(url)
-            .post('/api/' + _user_id + '/theme')
-            .send({
-                form_color: _form_color,
-                background_img: _background_img,
-                displayPhone: _displayPhone,
-                displayClock: _displayClock,
-                displaySignature: _displaySignature,
-                additionalComments: _additionalComments
-            })
-            .end(function(err, res) {
-                console.log(res.body);
-                res.body.should.have.property('user_id');
-                res.body.should.have.property('form_color');
-                res.body.should.have.property('background_img');
-                res.body.should.have.property('displayPhone');
-                res.body.should.have.property('displayClock');
-                res.body.should.have.property('displaySignature');
-                res.body.should.have.property('additionalComments');
+describe("Themes Route Test", function() {
+    var credentials;  // variable to hold all the need authentication variables.
 
-                res.body.user_id.should.equal(_user_id);
-                res.body.form_color.should.equal(_form_color);
-                res.body.background_img.should.equal(_background_img);
-                res.body.displayPhone.should.equal(_displayPhone);
-                res.body.displayClock.should.equal(_displayClock);
-                res.body.displaySignature.should.equal(_displaySignature);
-                res.body.additionalComments.should.equal(_additionalComments);
-
-                done();
-            });
+    // before function is called at the very beginning of the 'Forms' test suite,
+    // no tests are run until the done() callback is called.
+    before(function(done) {
+        // setupAdmin will create and admin and log you in, give it a callback that will give you
+        // the credentials you need. Make sure to call done() inside ConfigureAuth's callback!
+        ConfigureAuth.setupAdmin(function(cred) {
+            credentials = cred;
+            done();
+        });
     });
-});
-
-describe('GET /api/:user_id/theme', function() {
-    it('should respond with theme info for respective user_id', function(done) {
-        var url = "localhost:" + config.port;
-        var user_id = '1';
-        request(url)
-            .get('/api/' + user_id + '/theme')
-            .end(function(err, res) {
-                res.body.should.have.property('_id');
-                res.body.should.have.property('additionalComments');
-                res.body.should.have.property('user_id');
-                res.body.should.have.property('form_color');
-                res.body.should.have.property('background_img');
-                res.body.should.have.property('displayPhone');
-                res.body.should.have.property('displayClock');
-                res.body.should.have.property('displaySignature');
-
-                done();
-            });
-    });
-});
 
 
 
-describe('PUT /api/:user_id/theme', function() {
-    it('should respond with theme info for respective user_id settings that were updated', function(done) {
-        var url = "localhost:" + config.port;
-        var _user_id = '1';
-        var _form_color = "1";
-        var _background_img = "1";
-        var _displayPhone = false;
-        var _displayClock = false;
-        var _displaySignature = false;
-        var _additionalComments = false;
-        request(url)
-            .put('/api/' + _user_id + '/theme')
-            .send({
-                form_color: _form_color,
-                background_img: _background_img,
-                displayPhone: _displayPhone,
-                displayClock: _displayClock,
-                displaySignature: _displaySignature,
-                additionalComments: _additionalComments
-            })
-            .end(function(err, res) {
-                res.body.should.have.property('user_id');
-                res.body.should.have.property('form_color');
-                res.body.should.have.property('background_img');
-                res.body.should.have.property('displayPhone');
-                res.body.should.have.property('displayClock');
-                res.body.should.have.property('displaySignature');
-                res.body.should.have.property('additionalComments');
+                            describe('POST /api/:user_id/theme', function() {
+                                it('should respond with theme info for respective user_id settings that were created for first time user', function(done) {
+                                    var url = "localhost:" + config.port;
+                                    var _user_id = '1';
+                                    var _form_color = 'default';
+                                    var _background_img = 'default';
+                                    var _displayPhone = false;
+                                    var _displayClock = false;
+                                    var _displaySignature = false;
+                                    var _additionalComments = false;
+                                    request(url)
+                                        .post('/api/' + _user_id + '/theme')
+                                        .query({email: credentials.email, token: credentials.token})
+                                        .send({
+                                            form_color: _form_color,
+                                            background_img: _background_img,
+                                            displayPhone: _displayPhone,
+                                            displayClock: _displayClock,
+                                            displaySignature: _displaySignature,
+                                            additionalComments: _additionalComments
+                                        })
+                                        .end(function(err, res) {
+                                            console.log(res.body);
+                                            res.body.should.have.property('user_id');
+                                            res.body.should.have.property('form_color');
+                                            res.body.should.have.property('background_img');
+                                            res.body.should.have.property('displayPhone');
+                                            res.body.should.have.property('displayClock');
+                                            res.body.should.have.property('displaySignature');
+                                            res.body.should.have.property('additionalComments');
 
-                res.body.user_id.should.equal(_user_id);
-                res.body.form_color.should.equal(_form_color);
-                res.body.background_img.should.equal(_background_img);
-                res.body.displayPhone.should.equal(_displayPhone);
-                res.body.displayClock.should.equal(_displayClock);
-                res.body.displaySignature.should.equal(_displaySignature);
-                res.body.additionalComments.should.equal(_additionalComments);
+                                            res.body.user_id.should.equal(_user_id);
+                                            res.body.form_color.should.equal(_form_color);
+                                            res.body.background_img.should.equal(_background_img);
+                                            res.body.displayPhone.should.equal(_displayPhone);
+                                            res.body.displayClock.should.equal(_displayClock);
+                                            res.body.displaySignature.should.equal(_displaySignature);
+                                            res.body.additionalComments.should.equal(_additionalComments);
 
-                done();
-            });
-    });
-});
+                                            done();
+                                        });
+                                });
+                            });
 
-describe('DELETE /api/:user_id/theme', function() {
-    it('should respond with successful delete', function(done) {
-        var url = "localhost:" + config.port;
-        var user_id = '1';
-        request(url)
-            .delete('/api/' + user_id + '/theme')
-            .end(function(err, res) {
-                console.log(res.body);
-                res.body.should.equal(1);
-                done();
-            });
-    });
-});*/
+                            describe('GET /api/:user_id/theme', function() {
+                                it('should respond with theme info for respective user_id', function(done) {
+                                    var url = "localhost:" + config.port;
+                                    var user_id = '1';
+                                    request(url)
+                                        .get('/api/' + user_id + '/theme')
+                                        .query({email: credentials.email, token: credentials.token})
+                                        .end(function(err, res) {
+                                            res.body.should.have.property('_id');
+                                            res.body.should.have.property('additionalComments');
+                                            res.body.should.have.property('user_id');
+                                            res.body.should.have.property('form_color');
+                                            res.body.should.have.property('background_img');
+                                            res.body.should.have.property('displayPhone');
+                                            res.body.should.have.property('displayClock');
+                                            res.body.should.have.property('displaySignature');
+
+                                            done();
+                                        });
+                                });
+                            });
+
+
+
+                            describe('PUT /api/:user_id/theme', function() {
+                                it('should respond with theme info for respective user_id settings that were updated', function(done) {
+                                    var url = "localhost:" + config.port;
+                                    var _user_id = '1';
+                                    var _form_color = "1";
+                                    var _background_img = "1";
+                                    var _displayPhone = false;
+                                    var _displayClock = false;
+                                    var _displaySignature = false;
+                                    var _additionalComments = false;
+                                    request(url)
+                                        .put('/api/' + _user_id + '/theme')
+                                        .query({email: credentials.email, token: credentials.token})
+                                        .send({
+                                            form_color: _form_color,
+                                            background_img: _background_img,
+                                            displayPhone: _displayPhone,
+                                            displayClock: _displayClock,
+                                            displaySignature: _displaySignature,
+                                            additionalComments: _additionalComments
+                                        })
+                                        .end(function(err, res) {
+                                            res.body.should.have.property('user_id');
+                                            res.body.should.have.property('form_color');
+                                            res.body.should.have.property('background_img');
+                                            res.body.should.have.property('displayPhone');
+                                            res.body.should.have.property('displayClock');
+                                            res.body.should.have.property('displaySignature');
+                                            res.body.should.have.property('additionalComments');
+
+                                            res.body.user_id.should.equal(_user_id);
+                                            res.body.form_color.should.equal(_form_color);
+                                            res.body.background_img.should.equal(_background_img);
+                                            res.body.displayPhone.should.equal(_displayPhone);
+                                            res.body.displayClock.should.equal(_displayClock);
+                                            res.body.displaySignature.should.equal(_displaySignature);
+                                            res.body.additionalComments.should.equal(_additionalComments);
+
+                                            done();
+                                        });
+                                });
+                            });
+
+                            describe('DELETE /api/:user_id/theme', function() {
+                                it('should respond with successful delete', function(done) {
+                                    var url = "localhost:" + config.port;
+                                    var user_id = '1';
+                                    request(url)
+                                        .delete('/api/' + user_id + '/theme')
+                                        .query({email: credentials.email, token: credentials.token})
+                                        .end(function(err, res) {
+                                            console.log(res.body);
+                                            res.body.should.equal(1);
+                                            done();
+                                        });
+                                });
+                            });
+
+
+        after(function(done) {
+            // give cleanupAuth the email of the admin user it created earlier.
+            ConfigureAuth.cleanupAuth(credentials.email, done);
+        });
+    }
+);
