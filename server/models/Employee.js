0ddef28b06e95e84dd6fead4e5f3ecfbb340d473
Dyan Mozlowski <dylan.mozlowski@gmail.com>
2015-02-28 17:10:43 -0800

    Modified Employee model to allow for employees to log in

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -2,16 +2,29 @@
 
 /* Require mongoose to interact with mongoDB */
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 /*
  * Employee schema
  */
 var employeeSchema = mongoose.Schema({
   name: String,
-  email: String,
+  email: {type: String, unique: true, index: true, required: true},
+  password: String,
   phone_number: String,
-   _admin_id: { type: Schema.Types.ObjectId, ref: 'AdminUser', required: true }
+   _admin_id: { type: Schema.Types.ObjectId, ref: 'Admin', required: true }
+   token: String
 });
+
+// checking if password is valid
+employeeSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+// generating a hash
+employeeSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
 
 
 module.exports = mongoose.model('employee', employeeSchema);
