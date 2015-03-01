d044e00d35b04074865085ce68b6eb3280f16f51
Angelique De Castro <angeliqueajdecastro@gmail.com>
2015-03-01 15:44:18 -0800

    Add Employee form

--- client/app/components/dashboard/doctors/directives/doctors-table.js
+++ client/app/components/dashboard/doctors/directives/doctors-table.js
@@ -27,20 +27,39 @@ angular.module('dashboard')
         }
     }
 
+    function generateEmployee(id){
+        var docName = Names[Math.floor(Math.random() * 3)];
+        var phoneNumber = Phone[Math.floor(Math.random() * 3)];
+        var docEmail = Email[Math.floor(Math.random() * 3)];
+
+        return {
+            id: id,
+            Name: docName,
+            PhoneNumber: phoneNumber,
+            Email: docEmail
+        }
+    }
+
     $scope.rowCollection = [];
 
     for (id; id < 5; id++) {
         $scope.rowCollection.push(generateRandomItem(id));
     }
 
+    //add employee info
+    $scope.addEmployee = function(row){
+        $scope.rowCollection.push(row);
+        id++;
+    };
+
     //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
     $scope.displayedCollection = [].concat($scope.rowCollection);
 
     //add to the real data holder
-    $scope.addRandomItem = function addRandomItem() {
+    /*$scope.addRandomItem = function addRandomItem() {
         $scope.rowCollection.push(generateRandomItem(id));
         id++;
-    };
+    };*/
 
     //remove to the real data holder
     $scope.removeItem = function removeItem(row) {
