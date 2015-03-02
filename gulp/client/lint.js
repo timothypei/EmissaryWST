f45827bce430adade979ab49ad1c18600985cb04
Eduardo Aguilar <e6aguilar@gmail.com>
2015-03-01 20:13:09 -0800

    added server live reload, ability to enfore linting, and refactored gulp tasks

---
+++ gulp/client/lint.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint:client', function() {
+  return gulp.src(['./client/app/**/*.js'])
+    .pipe(jshint('./client/.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
