7cdffbf2aa57c2e4f54c1e1c6e5470a223498815
Eduardo Aguilar <e6aguilar@gmail.com>
2015-03-01 18:09:06 -0800

    fixed backend task

--- gulp/server/backend.js
+++ gulp/server/backend.js
@@ -3,20 +3,13 @@ var  nodemon = require('gulp-nodemon'),
 
 var gulp = require('gulp');
 
-/* Use foreman to serve the server, allowing gulp-nodemon
- * to automatically access the .env file.
- */
-gulp.task('serve:backend', function() {
-  return gulp.src('')
-    .pipe(shell('nf run gulp backend'))
-});
 
 /* Serve the server. gulp-nodemon is used for automatic
  * restarting upon changes to *.js files, excluding tests.
  * Note: do not start directories with ./ in watch and ignore
  */
-gulp.task('backend', ['build:dev'], function() {
-  nodemon({
+gulp.task('serve:backend', function() {
+  return nodemon({
     script: 'server/app.js',
     watch: ['server/**/*.js'],
     ignore: ['server/test/**/*.js']
@@ -26,3 +19,10 @@ gulp.task('backend', ['build:dev'], function() {
   });
 });
 
+/* Use foreman to serve the server, allowing gulp-nodemon
+ * to automatically access the .env file.
+ */
+gulp.task('backend', ['build:dev'], function() {
+  return gulp.src('')
+    .pipe(shell('nf run gulp serve:backend'))
+});
