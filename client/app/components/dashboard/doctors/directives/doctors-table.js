ff3e30d97c679ffe5650d505a1f7bc2348f7445d
Angelique De Castro <angeliqueajdecastro@gmail.com>
2015-03-02 21:55:56 -0800

    dummy removal and navbar logo

--- client/app/components/dashboard/doctors/directives/doctors-table.js
+++ client/app/components/dashboard/doctors/directives/doctors-table.js
@@ -7,49 +7,14 @@ angular.module('dashboard')
       controllerAs: 'safeCtrl'
     };
   })
-  .controller('SafeController', ['$scope', function ($scope) {
-
-    var Names = ['David Fincher', 'Mardy Bum', 'Alex Turner', 'Haruki Murakami'];
-    var Email = ['powell@ucsd.edu', 'peter@ucsd.edu', 'robo@ucsd.edu', '112@ucsd.edu'];
-    var Phone = ["(626)342-0347", "(234)343-3489", "(232)342-0193", "(342)347-2938"];
-    var id = 1;
-
-    function generateRandomItem(id) {
-        var docName = Names[Math.floor(Math.random() * 3)];
-        var phoneNumber = Phone[Math.floor(Math.random() * 3)];
-        var docEmail = Email[Math.floor(Math.random() * 3)];
-
-        return {
-            id: id,
-            Name: docName,
-            PhoneNumber: phoneNumber,
-            Email: docEmail
-        };
-    }
-
-    function generateEmployee(id){
-        var docName = Names[Math.floor(Math.random() * 3)];
-        var phoneNumber = Phone[Math.floor(Math.random() * 3)];
-        var docEmail = Email[Math.floor(Math.random() * 3)];
-
-        return {
-            id: id,
-            Name: docName,
-            PhoneNumber: phoneNumber,
-            Email: docEmail
-        };
-    }
-
+  .controller('SafeController', ['$scope', '$window', function ($scope, $window) {
+// include root scope
     $scope.rowCollection = [];
     $scope.newField = {};
     $scope.editing = false;
 
-    for (id; id < 5; id++) {
-        $scope.rowCollection.push(generateRandomItem(id));
-    }
-
     //add employee info
-    $scope.addEmployee = function addEmployee(row){
+    $scope.addEmployee = function(row){
         $scope.rowCollection.push(row);
         $scope.row = {};
         id++;
@@ -58,11 +23,13 @@ angular.module('dashboard')
     //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
     $scope.displayedCollection = [].concat($scope.rowCollection);
 
+    //edit a row
     $scope.editRowCollection = function(field) {
         $scope.editing = $scope.rowCollection.indexOf(field);
         $scope.newField = angular.copy(field);
-    }
+    };
     
+    // save edit
     $scope.saveField = function(index) {
         if ($scope.editing !== false) {
             $scope.rowCollection[$scope.editing] = $scope.newField;
@@ -70,6 +37,7 @@ angular.module('dashboard')
         }       
     };
     
+    // cancel editing
     $scope.cancel = function(index) {
         if ($scope.editing !== false) {
             $scope.rowCollection[$scope.editing] = $scope.newField;
@@ -77,14 +45,9 @@ angular.module('dashboard')
         }       
     };
 
-    //add to the real data holder
-    /*$scope.addRandomItem = function addRandomItem() {
-        $scope.rowCollection.push(generateRandomItem(id));
-        id++;
-    };*/
-
     //remove to the real data holder
     $scope.removeItem = function removeItem(row) {
+        $window.alert("Remove patient from queue? <button>Yes</button><button>Cancel</button>");
         var index = $scope.rowCollection.indexOf(row);
         if (index !== -1) {
             $scope.rowCollection.splice(index, 1);
