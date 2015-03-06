ca7941930d7e7a94acf8168bcfa02d3354b3a511
Dyan Mozlowski <dylan.mozlowski@gmail.com>
2015-03-05 17:40:19 -0800

    Removed employee from auth validation middleware

--- server/config/validation.js
+++ server/config/validation.js
@@ -1,17 +1,11 @@
 var Authmodel = require('../models/Authmodel');
-var Employee = require('../models/Employee');
 
 
 module.exports = function(req, res, next) {
   if(!req.query.email)
     return res.sendStatus(401);
 
-  var user = Authmodel;
-  // Employee login feature
-  /*if(req.query.isEmployee)
-    user = Employee;*/
-
-  user.findOne({email: req.query.email}, function(err, user) {
+  Authmodel.findOne({email: req.query.email}, function(err, user) {
     // if there are any errors, return the error before anything else
     if(err || !user)
       return res.status(400).send(err);
