441cbc043b969cb86c2e6faf830796032dd3f76b
Kevin Zhang <ktzluke10@gmail.com>
2015-03-05 22:47:41 -0800

    fixed dashboard titles

--- client/app/components/dashboard/main/controllers/dashboard-controller.js
+++ client/app/components/dashboard/main/controllers/dashboard-controller.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('dashboard')
-	.controller('DashboardController', ['$scope', '$state', function ($scope, $state) {
+	.controller('DashboardController', ['$scope', '$rootScope', '$state', function ($scope, $rootScope, $state) {
 		$scope.title = $state.current.title;
+
+		//Event handler to change titles
+		$rootScope.$on('$stateChangeStart', 
+			function(event, toState, toParams, fromState, fromParams) { 
+				$scope.title = toState.title;
+			}
+		)
 	}]);
