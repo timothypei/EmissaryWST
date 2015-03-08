90817209db856f7dc61295f1757632bfce29adfd
Justin Lai <jwlai@ucsd.edu>
2015-03-08 00:32:43 -0800

    Can recreate the form with hardcoded data, but not with the GET requested data...Needs HELP

--- client/app/components/patientCheckin/checkin/controllers/checkin-controller.js
+++ client/app/components/patientCheckin/checkin/controllers/checkin-controller.js
@@ -16,9 +16,49 @@ angular.module('checkin')
       $scope.init = function(){
         CheckinService.getForms($rootScope.admin_id).success(
           function(data){
-            console.log("yay");
             console.log(data);
-            $scope.dat = data;
+          var data = {};
+          data.template = {
+  "form_id": 1,
+  "form_name": "My Form",
+  "form_fields": [
+    {
+      "field_id": 6,
+      "field_title": "Name",
+      "field_type": "textfield",
+      "field_placeholder": "Name",
+      "field_required": true,
+      "field_disabled": false
+    },
+    {
+      "field_id": 2,
+      "field_title": "Email",
+      "field_type": "textfield",
+      "field_placeholder": "Email",
+      "field_required": true,
+      "field_disabled": false
+    },
+    {
+      "field_id": 3,
+      "field_title": "New field - 3",
+      "field_type": "textfield",
+      "field_placeholder": "",
+      "field_required": true,
+      "field_disabled": false
+    },
+    {
+      "field_id": 4,
+      "field_title": "New field - 4",
+      "field_type": "textfield",
+      "field_placeholder": "",
+      "field_required": true,
+      "field_disabled": false
+    }
+  ],
+  "submitted": false
+};
+          
+          $scope.form = data.template;
             return data;
           })
         .error(function(err){
