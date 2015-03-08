863fb1d9a29a03d3ac0b765dfb45769001711bdb
Luoxiao <lul017@Ucsd.edu>
2015-03-07 18:11:18 -0800

    got data for Patient mode

--- client/app/components/patientCheckin/checkin/services/checkin-services.js
+++ client/app/components/patientCheckin/checkin/services/checkin-services.js
@@ -5,7 +5,7 @@ angular.module('checkin')
   	  this.getForms = function(id) {
   	  	console.log(id);
   	  	console.log("getForms");
-  	  	var url = '/api/form/template/company/'+id;
+  	  	var url = '/api/form/template/company/54f8f23546b787e8335980e7';
   	  	console.log(url);
         return $http.get(url);
       };
