deef8dcf7f48edf7d036802b556fcad667f3ec29
Luoxiao <lul017@Ucsd.edu>
2015-03-09 00:58:40 -0700

    added injector to registration

--- client/app/auth/services/auth-service.js
+++ client/app/auth/services/auth-service.js
@@ -26,4 +26,27 @@ angular.module('auth')
       return $http.post('/auth/login', payload);
     };
 
+    this.reg = function register(account) {
+
+      var payload = account;
+      console.log(payload);
+      /* On success grab token from payload and register it with
+       * TokenInjector
+       */
+      function success(payload) {
+        if(payload && payload.token){
+          TokenInjector.setToken(payload.token);
+        } else {
+          // TODO: what happens when token is not returned
+        }
+      }
+
+      function error(err) {
+        TokenInjector.setToken(undefined); // TODO: need to check this
+      }
+
+      /* Return promise to caller so they can call success and error too */
+      return $http.post('/auth/signup', payload);
+    };
+
   }]);
--- client/app/components/receptionistPortal/register/controllers/register-controller.js
+++ client/app/components/receptionistPortal/register/controllers/register-controller.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('register')
-  .controller('RegisterController', ['$scope','$location', 'RegisterService',function($scope,$location, RegisterService){
+  .controller('RegisterController', ['$scope','$rootScope','$location', 'AuthService',function($scope,$rootScope,$location, AuthService){
   		$scope.user = {email: '', password: '', company_name: '', company_phone_number: ''};
+      $scope.number='';
       $scope.pass = '';
       $scope.err=false;
       $scope.check = false;
       $scope.errorMessage='';
   		$scope.reg = function(){  
-        $scope.user.company_phone_number = $scope.user.company_phone_number.toString();
-            
+        console.log(typeof $scope.user.company_phone_number);
+        $scope.user.company_phone_number = $scope.number.toString();
         //Email, password, company name, or phone fields are empty
         if($scope.user.email=='' || $scope.user.password=='' || $scope.user.company_name=='' || 
             $scope.user.company_phone_number==''){
@@ -39,9 +40,12 @@ angular.module('register')
           return;
         }
         else{
-  		    RegisterService.reg($scope.user)
+  		    AuthService.reg($scope.user)
           //when the API call was a success
       	  .success(function(data){
+          $rootScope.token = data.token;
+           $rootScope.admin_id = data.admin_id;
+            console.log(data);
           $location.path('/thankyou');
           return data;
       	 })
