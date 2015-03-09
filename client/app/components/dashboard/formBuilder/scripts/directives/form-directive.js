25bc0e62894061d8bd751bd618a8bf3a75c304c4
Wesley Yao <Wes@Wesleys-MacBook-Air.local>
2015-03-09 00:20:14 -0700

    create form

--- client/app/components/dashboard/formBuilder/scripts/directives/form-directive.js
+++ client/app/components/dashboard/formBuilder/scripts/directives/form-directive.js
@@ -3,14 +3,27 @@
 angular.module('DashboardFormBuilderModule')
   .directive('formDirective', function () {
     return {
-      controller: function($scope){
+      controller: function($scope, FormService, $modal){
         $scope.submit = function(){
-          alert('Form submitted..');
-          $scope.form.submitted = true;
-        };
+          FormService.getForm().then(function (response) {
+            if(response.formExist === false) // doesnt exist make a new one
+            {
+              $scope.form.submitted = true;
+              FormService.formData.submitted = true;
 
-        $scope.cancel = function(){
-          alert('Form canceled..');
+              FormService.createNewForm(FormService.formData);
+            }
+            else // does exist ask to replace
+            {
+              //console.log("make sure!!!");
+
+              var modalInstance = $modal.open({
+                templateUrl: 'views/components/dashboard/formBuilder/views/replaceFormModal.html',
+                controller : 'CreateNewFormModalInstanceCtrl',
+                controllerAs : 'vm'
+              });
+            }
+          });
         };
       },
       templateUrl: 'views/components/dashboard/formBuilder/views/directive-templates/form/form.html',
