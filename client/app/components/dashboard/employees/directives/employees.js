73cbab8994d683d5af0e0c978ef150084ac73b38
Angelique De Castro <angeliqueajdecastro@gmail.com>
2015-03-11 02:36:45 -0700

    fix edit and add phone number format

--- client/app/components/dashboard/employees/directives/employees.js
+++ client/app/components/dashboard/employees/directives/employees.js
@@ -148,7 +148,13 @@ angular.module('dashboard')
     		resolve: {}
     	}).
     	result.then(function(result){
-    		$scope.rowCollection.unshift(result);
+            var phone = result.PhoneNumber;
+    		$scope.rowCollection.unshift(result = {
+                Name:result.Name,
+                PhoneNumber:'('+phone.substring(0,3)+') '+phone.substring(3,6)+'-'+phone.substring(6,10),
+                Email:result.Email
+            }
+            );
     	});
     }
 
