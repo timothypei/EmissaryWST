1334f2ed1cf2aa751caa9c526feb018e350124b8
Mandy Ngo <mandyngo27@gmail.com>
2015-03-11 22:12:24 -0700

    .

--- client/app/components/dashboard/employees/controllers/employee-modal-controller.js
+++ client/app/components/dashboard/employees/controllers/employee-modal-controller.js
@@ -4,12 +4,6 @@ angular.module('dashboard')
 	.controller('EmployeeModalController', function ($scope, $modalInstance) {
 		$scope.ok = function () {
 			$modalInstance.close($scope.modal);
-			////on click show it
-			//$("#toaster").fadeIn();
-			////5 second then hide it
-			//setTimeout(function() {
-			//	$("#toaster").fadeOut();
-			//}, 3000);
 		};
 
 		$scope.cancel = function () {
