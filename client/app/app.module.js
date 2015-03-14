2a832d39f3956d6b50f751eab57681797fa1d32a
Kevin Zhang <ktzluke10@gmail.com>
2015-03-14 12:49:05 -0700

    added routing fix

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -100,15 +100,16 @@ angular.module('robobetty',
         templateUrl: 'views/components/dashboard/themes/views/dashboardIndex.html'
       });
   })
-  .run(['$rootScope', '$injector', function($rootScope, $injector){
-    $injector.get("$http").defaults.transformRequest = function(data, headersGetter) 
-    { 
-      if ($rootScope.token) headersGetter()['token'] = $rootScope.token;
-      if ($rootScope.email) headersGetter()['email'] = $rootScope.email; 
-      if (data) 
-        { 
-            return angular.toJson(data); 
-          }
-    } 
+  .run(['$rootScope', '$state', function($rootScope, $state){
+    $rootScope.$on('$stateChangeSuccess', 
+    function(event, toState, toParams, fromState, fromParams){
+      if(!$rootScope.admin_id) {
+        if(toState.name != 'signin') {
+      // debugger
+          $state.go("signin");
+        }
+      }
+    });
+    
   }]);
 
