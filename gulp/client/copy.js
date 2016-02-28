da6d65b8477757e2bfa4cc5399c85a99cdda313b
Daniel Cazares <dccazare@ucsd.edu>
2016-02-27 21:35:23 -0800

    Removed mobile folder and fixed resource paths for settings.html and visitors.html

--- gulp/client/copy.js
+++ gulp/client/copy.js
@@ -1,46 +1,32 @@
 var gulp = require('gulp');
 
-/* This will copy all our assets i.e. assets folder
- * to the dist folder.
- */
+
 gulp.task('copy:assets', function () {
   return gulp.src('./client/assets/**')
     .pipe(gulp.dest('./dist/assets'));
 });
 
 gulp.task('copy:js', function () {
-  return gulp.src('./client/js/**/*.js')
+  return gulp.src('./client/assets/js/*.js')
      .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('copy:css', function () {
-  return gulp.src('./client/css/**/*.css')
+  return gulp.src('./client/assets/css/*.css')
      .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('copy:images', function () {
-  return gulp.src('./client/img/**')
+  return gulp.src('./client/assets/images/*.*')
      .pipe(gulp.dest('./dist/images'));
 });
 
 gulp.task('copy:views', function () {
-  return gulp.src('./client/views/*.html')
+  return gulp.src('./client/assets/views/*.html')
      .pipe(gulp.dest('./dist'));
 });
 
-
-/* This will copy all our bower dependencies
- * to the dist folder
- */
 gulp.task('copy:bower-components', function () {
   return gulp.src('./client/bower_components/**')
     .pipe(gulp.dest('./dist/bower_components/'));
 });
-
-
-
- //gulp.task('copy:views', function () {
- //return gulp.src([ './client/app/**/*.html'])
-  // .pipe(gulp.dest('./dist/views/'));
-//});
- //*/
--- mobile/www/ionic/custom.js
+++
@@ -1,9 +0,0 @@
-window.shouldRotateToOrientation = function(degrees) {
-    return true;
-};
-
-IS_MOBILE = true;
-IONIC = 'ionic';
-
-var socket_io_url = "https://blue-jay.herokuapp.com/socket.io/socket.io.js";
-document.write('<script src="'+socket_io_url+'"></script>');
