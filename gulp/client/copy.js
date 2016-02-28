9a60a8967929801a4cf9656a4cc36f9276670582
Daniel Cazares <dccazare@ucsd.edu>
2016-02-27 22:31:34 -0800

    Added sign up page.

--- gulp/client/copy.js
+++ gulp/client/copy.js
@@ -7,22 +7,22 @@ gulp.task('copy:assets', function () {
 });
 
 gulp.task('copy:js', function () {
-  return gulp.src('./client/assets/js/*.js')
+  return gulp.src('./client/assets/js/**/*.js')
      .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('copy:css', function () {
-  return gulp.src('./client/assets/css/*.css')
+  return gulp.src('./client/assets/css/**/*.css')
      .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('copy:images', function () {
-  return gulp.src('./client/assets/images/*.*')
+  return gulp.src('./client/assets/images/**/*.*')
      .pipe(gulp.dest('./dist/images'));
 });
 
 gulp.task('copy:views', function () {
-  return gulp.src('./client/assets/views/*.html')
+  return gulp.src('./client/assets/views/**/*.html')
      .pipe(gulp.dest('./dist'));
 });
 
