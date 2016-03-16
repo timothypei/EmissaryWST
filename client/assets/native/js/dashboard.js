ce0f121f467ff513be4204308b52da28a8556cb3
yojeon <younoda@gmail.com>
2016-03-16 16:25:51 -0700

    dropdown added need css work

--- client/assets/native/js/dashboard.js
+++ client/assets/native/js/dashboard.js
@@ -54,12 +54,12 @@ $(document).ready(function(){
     //SOCKET LISTEN FOR VISITOR QUEUE
     socket.on(VISITOR_LIST_UPDATE, function (data) {
         if(DEBUG)console.log("VISITOR_LIST_UPDATE");
-
+        visitorList = data.visitors;
         //Parse Visitor List to format Date
         for(var i = 0, len = visitorList.length; i< len; i++){
             visitorList[i].checkin_time = formatTime(visitorList[i].checkin_time);
         }
-        for(i = 0, i < len; i++){
+        for(i = 0; i < len; i++){
           var appList = visitorList[i].appointments;
           for(var j = 0, appLen = appList.length; j < appLen; j++){
             if(appList[j]._id == visitorList[i]._id){
