b879fcb613ee09533d4ec5c6f4bf281a885c0188
yojeon <younoda@gmail.com>
2016-03-15 19:42:09 -0700

    error message on login

--- client/assets/native/js/login.js
+++ client/assets/native/js/login.js
@@ -3,7 +3,9 @@ $(function() {
     $('#loginButton').click(function () {
         var userData = grabUserData();
         //alert(userData);
+        event.preventDefault();
         ajaxPostUser('/api/employees/login', userData);
+
 
         
     });
@@ -26,11 +28,17 @@ function ajaxPostUser(url, data){
         dataType: 'json',
         success: function(response){
             console.log(response);
-            
+
             localStorage.setItem('currentUser', JSON.stringify(response));
             //alert(response.company_id);
             ajaxGetCompanyInfo('/api/companies/' + response.company_id);
-        }
+        },
+        error: function() {
+
+            window.onerror=handleError();
+            event.preventDefault();
+            //location.href = '/login.html';
+         }
     });
 }
 // ex) company_id : 56e8a51293a19986040e93fe
@@ -57,4 +65,12 @@ function grabUserData(){
     user.email = $('#username').val();
     user.password = $('#password').val();
     return user;
+}
+
+
+
+function handleError()
+{
+    errorlog.innerHTML+="Not Valid Username and Password, please type valid one.";
+    return true;
 }
